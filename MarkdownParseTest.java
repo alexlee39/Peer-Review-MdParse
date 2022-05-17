2af3acc095dceb9599d277c1b8a2c1a1af0e4a48
Alexander Lee <alexanderlee014@gmail.com>
2022-05-16 21:54:27 -0700

    Added 3 more MD Files and TestCases

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -48,4 +48,39 @@ public class MarkdownParseTest {
     public void failTest() {
         assertEquals(2, 1+1);
     }
+
+    @Test
+    public void testSnip1(){
+        // Path fileName = Path.of("Snippet1.md");
+        // String content = Files.readString(fileName);
+        // ArrayList<String> links = MarkdownParse.getLinks(content);
+        // assertEquals(List.of("`google.com","google.com","ucsd.edu"), links);
+        try{
+            assertEquals(List.of("`google.com","google.com","ucsd.edu"),
+            MarkdownParse.getLinks(Files.readString(Path.of("Snippet1.md"))));
+            } catch (IOException e){
+                fail();
+            }
+    }
+
+    @Test
+    public void testSnip2(){
+        try{
+            assertEquals(List.of("a.com","a.com(())","example.com"),
+            MarkdownParse.getLinks(Files.readString(Path.of("Snippet2.md"))));
+            } catch (IOException e){
+                fail();
+            }
+    }
+
+    @Test 
+    public void testSnip3(){
+        try{
+            assertEquals(List.of("https://www.twitter.com",
+                "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-202s2/schedule",
+            "https://cse.ucsd.edu/"),MarkdownParse.getLinks(Files.readString(Path.of("Snippet3.md"))));
+            } catch (IOException e){
+                fail();
+            }
+    }
 }
